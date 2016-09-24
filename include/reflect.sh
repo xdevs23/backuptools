c12efc8425db82f1d5c3922bdb703e72b0084c9e
Simao Gomes Viana <xdevs23@outlook.com>
2016-09-24 11:21:53 +0200

    include/reflect: improve reflection

--- include/reflect.sh
+++ include/reflect.sh
@@ -13,22 +13,39 @@ function reflectorId() {
   export REFLECTOR_ID="$1"
 }
 
+
+rfl_break_oo=0
+rfl_nobreak_oo=1
+
 function run() {
-  reflectresult=0
-  allargs="$@"
-  reflector="$1"
-  reflectargs="${allargs//$reflector /}"
+  run_internal $rfl_break_oo $@
+}
+
+function runall() {
+  run_internal $rfl_nobreak_oo $@
+}
+
+function run_internal() {
+  breakoo="$1"
+  shift 1
+  local reflectresult=0
+  local allargs="$@"
+  local reflector="$1"
+  local reflectargs="${allargs//$reflector /}"
   for sc in $(find $REFLECTORS_PATH/ -type f); do
+    local reflfn="${sc/$REFLECTOR_PATH\//}"
     source $sc
-    reflectresult=$?
+    local reflectresult=$?
     if [ ! $reflectresult -eq 0 ]; then
-      echo -e "\033[91mReflector ${sc/$REFLECTOR_PATH\//} is invalid or failed!\033[0m"
+      echo -e "\033[91mReflector $reflfln is invalid or failed!\033[0m"
       break
     fi
     if [ "$REFLECTOR_NAME" == "$reflector" ] ||
        [ "$REFLECTOR_ID"   == "$reflector" ]; then
       reflect $reflectargs
-      break
+      if [ $breakoo -eq $rfl_break_oo ]; then
+        break
+      fi
     fi
   done
   clean_reflection
