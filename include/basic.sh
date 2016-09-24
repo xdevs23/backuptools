f87f7dbc023a625b71d0971ba22808760884ddbb
Simao Gomes Viana <xdevs23@outlook.com>
2016-09-24 14:24:46 +0200

    Add support for help text

--- include/basic.sh
+++ include/basic.sh
@@ -25,11 +25,24 @@ function getdynamic() {
   echo -n ${!varname}
 }
 
+function cmdHelpText() {
+  logd "Associating help text with reflector $REFLECTOR_ID"
+  local tcommand="$(getdynamic COMMAND_${REFLECTOR_ID}_ASSOC_CMD)"
+  local command="${tcommand/-/___DASH___}"
+  if [ ! -z "$(getdynamic COMMAND_${command}_HAS_HELP)" ]; then
+    logd "Help text of command $tcommand already associated."
+    return 0
+  fi
+  declaredynamic COMMAND_${command}_HAS_HELP 1
+  export MAIN_COMMANDS_HELP="$MAIN_COMMANDS_HELP$tcommand      $@\n"
+}
+
 function associateCmd() {
   logd "Associating command $1 with reflector $REFLECTOR_ID"
-  local command="$1"
-  local command="${command/-/___DASH___}"
+  local tcommand="$1"
+  local command="${tcommand/-/___DASH___}"
   declaredynamic COMMAND_${command}_RUNPOINT $REFLECTOR_ID
+  declaredynamic COMMAND_${REFLECTOR_ID}_ASSOC_CMD $tcommand
 }
 
 function getReflectorForCmd() {
@@ -58,6 +71,8 @@ include reflect
 include colors
 include precheck
 include product_config
+
+export MAIN_COMMANDS_HELP=""
 
 doprecheck
 if [ ! $? -eq 0 ]; then
